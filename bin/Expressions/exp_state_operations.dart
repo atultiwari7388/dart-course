a98355f0264fcb376915b810a8465616f3f6860d
Atul Tiwari <tiwariatul9526@gmail.com>
2021-09-22 14:40:08 +0530

    Statements, Expressions, Operations

---
+++ bin/Expressions/exp_state_operations.dart
@@ -0,0 +1,20 @@
+// Statement is instructions
+
+void main() {
+//Statement
+  print("Hello");
+  print("Hello 2");
+
+// Expressions are things the do nothing
+
+  4;
+  "Do Nothing";
+
+// operations
+
+  print(1 + 1);
+  print(2 - 1);
+  print(1 * 1);
+  print(1 / 1);
+  print(1 ~/ 1);
+}
