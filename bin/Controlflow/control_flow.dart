90c4265c6ac75e9b157a1df60402ee0a042b3e1f
Atul Tiwari <tiwariatul9526@gmail.com>
2021-09-28 16:17:01 +0530

    control flow

--- bin/Controlflow/control_flow.dart
+++ bin/Controlflow/control_flow.dart
@@ -1,1 +1,32 @@
-void main() {}
+void main() {
+// If Statement
+/** 
+ * If statement, which allows the program to do something only if a certain condition is true
+ */
+
+  const age = 15;
+  if (age >= 18) {
+    print("You are under age");
+  }
+
+  // else clause
+
+  if (age <= 18) {
+    print("You can drink !");
+  } else {
+    print("You can't drink !");
+  }
+
+  // else if cause
+
+  if (age > 18 && age >= 21) {
+    print("You are of age, but you can't drink");
+  } else if (age > 18 && age >= 21) {
+    print("You are of age , and can drink");
+  } else {
+    print("Just drink water");
+  }
+
+// variable scope
+// where is the variable visible and usable
+}
