46293fc3c1f9789e33e772f9ae21072916923da9
Atul Tiwari <tiwariatul9526@gmail.com>
2021-10-18 13:29:33 +0530

    dart basics

--- bin/testing.dart
+++ bin/testing.dart
@@ -1,18 +1,4 @@
 void main() {
-  // var name = "Atul Tiwari";
-  // var age = 20;
-  // var height = 1.4;
-
-  // final person1 = describe(name: name, age: age, height: height);
-  // final person2 = describe2(name: "Murari", age: 21, height: 1.2);
-  // print(person1);
-  // print(person2);
-  // sayName(name);
-
-// class
-  // final person = Person(name: "Atul", age: 20, height: 1.2);
-  // print(person.describe());
-
 //inheritance
   final employee = Employee(
       taxCode: "AWT004", salary: 25000, name: "Atul", age: 20, height: 1.2);
@@ -55,14 +41,3 @@ class Employee extends Person {
   String toString() =>
       "${super.toString()}, taxCode: $taxCode, age: $age, salary: $salary";
 }
-
-// functions in dart.
-
-// String describe({String? name, int? age, double? height}) {
-//   return "Hello Mr. $name. My name has ${name!.length} letters. I'm $age years old.  I'm $height meters tall";
-// }
-
-// String describe2({String? name, int? age, double? height}) =>
-//     "Hello Mr. $name, My name has ${name!.length} letters. I'm $age years old', I'm $height years old";
-
-// void sayName(String name) => print("Hello i'm $name");
